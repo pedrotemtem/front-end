750bb5259cbaa71916a24a17f3c102754e8644ce
Pedro Reis <pedro.reis@appdetex.com>
2022-11-22 15:25:32 +0000

    adds analyst id parameter

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -7,8 +7,8 @@ export default class Welcome extends Component {
     render() {
         return (
             <div className="bigDiv">
-                <div className="welcome"> Welcome, <span className="analystName">{this.props.params.name}</span> !</div>
-                <div className="redirect"> To check your detections, please <Link to="/detections">click here</Link></div>
+                <div className="welcome"> Welcome, <span className="analystName">{this.props.params.name["name"]}</span> !</div>
+                <div className="redirect"> To check your detections, please <Link to="/detections/">click here</Link></div>
             </div>
 
         )
