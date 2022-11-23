74873fc7c94f45d72af22c55445e49336c14ffc8
Pedro Temtem <pedro.temtem@appdetex.com>
2022-11-23 17:46:39 +0000

    update commented

--- src/api/DetectionsTable.jsx
+++ src/api/DetectionsTable.jsx
@@ -174,7 +174,7 @@ class DetectionsTable extends React.Component {
                                 alert(error);
                             }
                             )
-                        }} 
+                        }}
                     />
                 </Box>
 
@@ -195,6 +195,32 @@ class DetectionsTable extends React.Component {
             
             </>
     )}
+    
+    /*useEffect(){
+      // PUT request using fetch with error handling
+      const requestOptions = {
+          method: 'PUT',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ title: 'React Hooks PUT Request Example' })
+      };
+      fetch('https://jsonplaceholder.typicode.com/invalid-url', requestOptions)
+          .then(async response => {
+              const data = await response.json();
+  
+              // check for error response
+              if (!response.ok) {
+                  // get error message from body or default to response status
+                  const error = (data && data.message) || response.status;
+                  return Promise.reject(error);
+              }
+  
+              setPostId(data.id);
+          })
+          .catch(error => {
+              setErrorMessage(error);
+              console.error('There was an error!', error);
+          });
+    }*/
 
     saveInDatabase(){
         console.log('Success')
