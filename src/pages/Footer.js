9384d0d9f62cb9c5aab12f040aabd53a7c8652b0
Pedro Reis <pedro.reis@appdetex.com>
2022-11-25 11:20:01 +0000

    finalizes login and welcome component

---
+++ src/pages/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./pagesCSS/Footer.css";
+
+export default class Footer extends React.Component {
+    render() {
+        return (
+            <div className="footer">Tracing ™</div>
+        )
+    }
+}
