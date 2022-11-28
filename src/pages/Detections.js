7be3ce0b2f0dc6a92fc1c24a4c0056acf616d137
Pedro Reis <pedro.reis@appdetex.com>
2022-11-28 16:36:17 +0000

    changing in detections are now being saved in the db

--- src/pages/Detections.js
+++ src/pages/Detections.js
@@ -1,244 +1,374 @@
 import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { DataGridPro } from '@mui/x-data-grid-pro';
+import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
-import {Button} from "@mui/material";
-import './pagesCSS/DetectionsTable.css';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import CancelIcon from '@mui/icons-material/Close';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-const columns = [
-    { field: 'id', headerName: 'ID', width: 40 },
-    {
-      field: 'title',
-      headerName: 'Title',
-      width: 150,
-    },
-    {
-      field: 'description',
-      headerName: 'Description',
-      width: 150,
-      description: 'This column has a value getter and is not sortable.',
-    },
-    {
-      field: 'url',
-      headerName: 'Url',
-      type: 'number',
-      width: 110,
-    },
-    {
-      field: 'image_url',
-      headerName: 'Detection Image',
-      sortable: false,
-      width: 150,
-      renderCell: (params) =><img src={params.value} alt="" className="image" height="50px"/>,
-    },
-    {
-      field: 'price',
-      headerName: 'Price',
-      width: 150,
-    },
-    {
-      field: 'paid_search',
-      headerName: 'Paid Search',
-      width: 110,
-    },
-    {
-      field: 'order_on_page',
-      headerName: 'Order On Page',
-      sortable: true,
-      type: 'number',
-      width: 130,
-    },
-    {
-      field: 'capture_date',
-      headerName: 'Capture DateTime',
-      width: 150,
-    },
-    {
-      field: 'seller',
-      headerName: 'Seller',
-      width: 110,
-    },
-    {
-      field: 'marketplace',
-      headerName: 'Marketplace',
-      sortable: false,
-      width: 160,
-    },
-  
-    {
-      field: 'status',
-      headerName: 'Status',
-      sortable: true,
-      width: 160,
-      editable: true,
-      type: 'singleSelect',
-      valueOptions: ['test','test2']
-    },
-    {
-      field: 'state',
-      headerName: 'State',
-      width: 110,
-      editable: true,
-    },
-    {
-      field: 'reason_codes',
-      headerName: 'Reason',
-      sortable: false,
-      width: 160,
-      editable: true,
-    },
-  ];
-  
-  const columnsAudit = [
-      {
-          field: 'id',
-          headerName: 'ID',
-          width: 110,
-      },
-  
-      {
-          field: 'date_time',
-          headerName: 'Date time',
-          width: 110,
-      },
-  
-      {
-          field: 'parameter',
-          headerName: 'Parameter',
-          width: 110,
-      },
-  
-      {
-          field: 'marketplace_detections_id',
-          headerName: 'Detection Id',
-          width: 110,
-      },
-      
-      {
-          field: 'analysts_id',
-          headerName: 'Analyst Id',
-          width: 110,
-      },
-  
-  ];
+import { useEffect, useState } from 'react';
+import {
+  GridRowModes,
+  DataGridPro,
+  GridToolbarContainer,
+  GridActionsCellItem,
+} from '@mui/x-data-grid-pro';
+
+export default function FullFeaturedCrudGrid() {
+  const [rows = [], setRows] =useState();
+  const [rows2= [], setRows2] = useState();
+
+  const [rowModesModel, setRowModesModel] = React.useState({});
+  const [rowsId= [], setRowsId] = useState();
+  const [rowsAudit=[], setRowsAudit]= useState();
+  const [allData =[], setAllData]=useState();
+
+  var rowsIdforAudit= 0;
+
+  var rowToSave = {id: 0, state: "", status: "", reason_code: ""}
+
+  var newStatus= ""
+  var newState= ""
+  var newReasonCodes=""
 
 
-export default class DetectionsTable extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            detections: [],
-            detectionId: 0,
-            audit: []
+  const getAllDetections = async () => {
+    const response = await fetch("http://localhost:8008/api/marketplacedetections/getAll")
+    .then((response) => response.json());
+
+    setRows(response)
+    setRows2(response)
+  }
+  
+
+  useEffect(() => {
+    getAllDetections();
+    },[])
+
+  const handleRowEditStart = (params, event) => {
+    event.defaultMuiPrevented = true;
+  };
+
+  const handleRowEditStop = (params, event) => {
+    event.defaultMuiPrevented = true;
+  };
+
+  const handleEditClick = (id) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+  };
+
+  const handleSaveClick = (id) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+    /* call api update detection*/
+    
+    }
+
+    const updateDetection=() => rowsId.forEach(element =>{
+        const detect= rows.find(detectionElement =>{
+            return detectionElement.id === element
+        })
+
+        const detect2= rows2.find(detectionElement =>{
+            return detectionElement.id === element
+        })
+
+        if (detect2.state === detect.state) {
+            newState = ""
         }
-    }
-    
-    componentDidMount() {
-        fetch("http://localhost:8008/api/marketplacedetections/getAll")
-        .then(res => res.json())
-        .then(
-            (detections) => {
-                this.setState({ detections: detections });
-            },
-            (error) => {
-                alert(error);
+        else{ newState= detect.state}
+
+        if (detect2.status === detect2.status) {
+            newStatus = ""
+        }
+        else{ newStatus = detect.status;}
+
+        if (detect2.reason_codes === detect.reason_code) {
+            newReasonCodes = "";
+        }
+        else{ newReasonCodes= detect.reason_code;}
+        
+        rowToSave = {id: element, state: newState, status: newStatus, reason_code: newReasonCodes};
+
+        fetch('http://localhost:8008/api/marketplacedetections/update', {
+            method: 'PUT',
+            body: JSON.stringify(rowToSave),
+            headers: {
+              'Content-Type': 'application/json',
+              "Access-Control-Allow-Origin": "*",
             }
-        )
+        })
+        console.log(rowToSave);
+    });  
+
+
+  const handleDeleteClick = (id) => () => {
+    setRows(rows.filter((row) => row.id !== id));
+  };
+
+  const handleCancelClick = (id) => () => {
+    setRowModesModel({
+      ...rowModesModel,
+      [id]: { mode: GridRowModes.View, ignoreModifications: true },
+    });
+
+    const editedRow = rows.find((row) => row.id === id);
+    if (editedRow.isNew) {
+      setRows(rows.filter((row) => row.id !== id));
     }
+  };
 
-    render() {
-        return (
-            <>
-                <Box sx={{ height: 650, width: '100%' }}>
-                    <DataGridPro
-                        rows={this.state.detections}
-                        columns={columns}
-                        pagination
-                        pageSize={10}
-                        rowsPerPageOptions={[10]}
-                        checkboxSelection
-                        disableSelectionOnClick
-                        experimentalFeatures={{ newEditingApi: true }}
-                        /*onCellClick={detectionRow => {
-                            this.state.detectionId= detectionRow.id;
-                            fetch(`Colocar isto com o detectionId/${this.state.detectionId}`)
-                            .then(res => res.json())
-                            .then(
-                            (detectionAudit) => {
-                                this.setState({ detectionAudit : detectionAudit });
-                            },
-                            (error) => {
-                                alert(error);
-                            }
-                            )
-                        }}*/
-                        onSelectionModelChange = {id => {
-                            this.setState({
-                                detectionId: id
-                            })
-                          console.log(this.state.detectionId)
-                        }}
-                    />
+  const processRowUpdate = (newRow) => {
+    const updatedRow = { ...newRow, isNew: false };
+    setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
+    return updatedRow;
+  };
 
-                </Box>
+  const columnsAudit = [
+    {
+        field: 'id',
+        headerName: 'ID',
+        width: 110,
+    },
 
-                <Button variant="outlined" onClick={this.saveInDatabase}> Update </Button>
-            
-                <Box sx={{ height: 400, width: '100%' }}>
-                    <DataGridPro
-                        rows={this.state.audit}
-                        columns={columnsAudit}
-                        pagination
-                        pageSize={5}
-                        rowsPerPageOptions={[5]}
-                        checkboxSelection
-                        disableSelectionOnClick
-                        experimentalFeatures={{ newEditingApi: true }}
-                    />
-                </Box>
-            
-            
-            </>
-    )}
+    {
+        field: 'date_time',
+        headerName: 'Date time',
+        width: 110,
+    },
+
+    {
+        field: 'parameter',
+        headerName: 'Parameter',
+        width: 110,
+    },
+
+    {
+        field: 'marketplace_detections_id',
+        headerName: 'Detection Id',
+        width: 110,
+    },
     
-    /*useEffect(){
-      // PUT request using fetch with error handling
-      const requestOptions = {
-          method: 'PUT',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ title: 'React Hooks PUT Request Example' })
-      };
-      fetch('https://jsonplaceholder.typicode.com/invalid-url', requestOptions)
-          .then(async response => {
-              const data = await response.json();
-  
-              // check for error response
-              if (!response.ok) {
-                  // get error message from body or default to response status
-                  const error = (data && data.message) || response.status;
-                  return Promise.reject(error);
-              }
-  
-              setPostId(data.id);
-          })
-          .catch(error => {
-              setErrorMessage(error);
-              console.error('There was an error!', error);
-          });
-    }*/
+    {
+        field: 'analysts_id',
+        headerName: 'Analyst Id',
+        width: 110,
+    },
 
-    saveInDatabase(){
-        console.log('Success')
-        console.log('Success2')
+    ];
 
-    }
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+        field: 'title',
+        headerName: 'Title',
+        width: 150,
+    },
+    {
+        field: 'description',
+        headerName: 'Description',
+        width: 150,
+        description: 'This column has a value getter and is not sortable.',
+    },
+    {
+        field: 'url',
+        headerName: 'Url',
+        type: 'number',
+        width: 110,
+    },
+    {
+        field: 'image_url',
+        headerName: 'Image Url',
+        sortable: false,
+        width: 200,
+        renderCell: (params) =><img src= {params.value} className="image" height="50px"/>,
+    },
+    {
+        field: 'price',
+        headerName: 'Price',
+        width: 150,
+    },
+    {
+        field: 'paid_search',
+        headerName: 'Paid Search',
+        width: 110,
+    },
+    {
+        field: 'order_on_page',
+        headerName: 'Order On Page',
+        sortable: true,
+        type: 'number',
+        width: 160,
+    },
+    {
+        field: 'capture_date',
+        headerName: 'Capture Date',
+        width: 150,
+    },
+    {
+        field: 'seller',
+        headerName: 'Seller',
+        width: 110,
+    },
+    {
+        field: 'marketplace',
+        headerName: 'Marketplace',
+        sortable: false,
+        width: 160,
+    },
 
+    {
+        field: 'status',
+        headerName: 'Status',
+        sortable: true,
+        width: 160,
+        editable: true,
+        type: 'singleSelect',
+        valueOptions: ['open','closed']
+    },
+    {
+        field: 'state',
+        headerName: 'State',
+        width: 110,
+        editable: true,
+        type: 'singleSelect',
+        valueOptions: ['new','benign','enforce','']
+    },
+    {
+        field: 'reason_code',
+        headerName: 'Reason',
+        sortable: false,
+        width: 160,
+        editable: true,
+        type: 'singleSelect',
+        valueOptions: ['test','test2']
+    },
+    {
+      field: 'actions',
+      type: 'actions',
+      headerName: 'Actions',
+      width: 100,
+      cellClassName: 'actions',
+      getActions: ({ id }) => {
+        const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
 
+        if (isInEditMode) {
+          return [
+            <GridActionsCellItem
+              icon={<SaveIcon />}
+              label="Save"
+              onClick={handleSaveClick(id)}
+            />,
+            <GridActionsCellItem
+              icon={<CancelIcon />}
+              label="Cancel"
+              className="textPrimary"
+              onClick={handleCancelClick(id)}
+              color="inherit"
+            />,
+          ];
+        }
 
+        return [
+          <GridActionsCellItem
+            icon={<EditIcon />}
+            label="Edit"
+            className="textPrimary"
+            onClick={handleEditClick(id)}
+            color="inherit"
+          />,
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={handleDeleteClick(id)}
+            color="inherit"
+          />,
+        ];
+      },
+    },
+  ];
+
+  return (
+    <>
+    <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Instructions</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            To edit a detection, click on the pencil symbol in the Actions column. After editing, click on the Save Button.
+            After editing all the pretended detections, select the detections you edited (column on the left of Id), and then click the "Update" button that is bellow the table.
+            This way, your detections will be saved in the database.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+    <><Box
+          sx={{
+              height: 500,
+              width: '100%',
+              '& .actions': {
+                  color: 'text.secondary',
+              },
+              '& .textPrimary': {
+                  color: 'text.primary',
+              },
+          }}
+      >
+          <DataGridPro
+              rows={rows}
+              columns={columns}
+              editMode="row"
+              checkboxSelection
+              disableSelectionOnClick
+              rowModesModel={rowModesModel}
+              onRowModesModelChange={(newModel) => setRowModesModel(newModel)}
+              onRowEditStart={handleRowEditStart}
+              onRowEditStop={handleRowEditStop}
+              processRowUpdate={processRowUpdate}
+              /*onCellClick={detectionRow => {
+                  rowsIdforAudit = detectionRow.id;
+                  fetch(`Colocar isto com o detectionId/${rowsIdforAudit}`)
+                      .then(res => res.json())
+                      .then(
+                          (detectionAudit) => {
+                              setRowsAudit({ detectionAudit: detectionAudit });
+                          },
+                          (error) => {
+                              alert(error);
+                          }
+                      );
+              } }*/
+              onSelectionModelChange={rowsId => {setRowsId(rowsId); console.log(rowsId)}}
+              experimentalFeatures={{ newEditingApi: true }}
+              />
+      </Box>
+
+      <Button variant="outlined" onClick={()=> {console.log(allData);
+                                            updateDetection();}}> Update </Button>
+        
+          <Box sx={{ height: 400, width: '100%' }}>
+            <DataGridPro
+                rows={rowsAudit}
+                columns={columnsAudit}
+                pagination
+                pageSize={5}
+                rowsPerPageOptions={[5]}
+                checkboxSelection
+                disableSelectionOnClick
+                experimentalFeatures={{ newEditingApi: true }}
+            />
+        </Box>
+    </></>
+  );
 }
